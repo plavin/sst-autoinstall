808066ed0cf1dee3153a1c7e5638a0b0b94a904e
Patrick Lavin <plavin997@gmail.com>
2021-08-16 15:08:47 -0400

    put URLs in separate file

---
+++ configure_urls.sh
@@ -0,0 +1,10 @@
+# Go to https://software.intel.com/content/www/us/en/develop/articles/pin-a-binary-instrumentation-tool-downloads.html and copy the link to the "Kit" you would like to use for your system
+PINURL='https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.20-98437-gf02b61307-gcc-linux.tar.gz'
+
+# Go to https://cmake.org/download/ and pick the binary distribution for your platform. Get the .tar.gz, not the .sh file.
+CMAKEURL=https://github.com/Kitware/CMake/releases/download/v3.21.1/cmake-3.21.1-linux-x86_64.tar.gz
+
+# Pick your sst repo
+COREREPO=git@github.com:sstsimulator/sst-core.git
+ELEMENTSREPO=git@github.com:sstsimulator/sst-elements.git
+DRAMSIM3REPO=git@github.com:umd-memsys/DRAMsim3.git
--- install_sst.sh
+++ install_sst.sh
@@ -3,16 +3,8 @@ SRCDIR=$TOP/source
 INSTALLDIR=$TOP/install
 DEFFILE=$TOP/sstdefs.sh
 
-# Go to https://software.intel.com/content/www/us/en/develop/articles/pin-a-binary-instrumentation-tool-downloads.html and copy the link to the "Kit" you would like to use for your system
-PINURL='https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.20-98437-gf02b61307-gcc-linux.tar.gz'
-
-# Go to https://cmake.org/download/ and pick the binary distribution for your platform. Get the .tar.gz, not the .sh file.
-CMAKEURL=https://github.com/Kitware/CMake/releases/download/v3.21.1/cmake-3.21.1-linux-x86_64.tar.gz
-
-# Pick your sst repo
-COREREPO=git@github.com:sstsimulator/sst-core.git
-ELEMENTSREPO=git@github.com:sstsimulator/sst-elements.git
-DRAMSIM3REPO=git@github.com:umd-memsys/DRAMsim3.git
+# Pull In Urls
+. configure_urls.sh
 
 echo "Source code will be placed in $SRCDIR"
 echo "Programs will be installed in $INSTALLDIR"
@@ -96,8 +88,8 @@ cd sst-elements
 find . -name Makefile.am -exec sed -i s'/-Werror//g' {} \;
 ./autogen.sh
 ./configure --prefix=$SST_ELEMENTS_HOME --with-sst-core=$SST_CORE_HOME --with-pin=$PIN_HOME --with-dramsim3=$DRAMDIR
-make all -j8
-make install
+#make all -j8
+#make install
 cd $TOP
 
 echo "Done"
